57a30ec4ae5c657b3110a5b680e0a3905c3dc0b8
Sam <samuel.goebert@bigcurl.de>
2025-09-29 08:35:18 +0200

    Added an error handling test suite that verifies the parser rejects documents without a LENEX root element and wraps Nokogiri syntax errors in Lenex::Parser::ParseError.
    Exercised the private ensure_io path to confirm IO sources are returned and placed into binary mode before parsing.

---
+++ test/lenex_parser_error_handling_test.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require 'stringio'
+require 'test_helper'
+
+class LenexParserErrorHandlingTest < Minitest::Test
+  NON_LENEX_ROOT_XML = <<~XML
+    <DOCUMENT>
+      <CONSTRUCTOR name="Builder" registration="Example Org" version="1.2.3">
+        <CONTACT email="support@example.com" />
+      </CONSTRUCTOR>
+    </DOCUMENT>
+  XML
+  private_constant :NON_LENEX_ROOT_XML
+
+  MALFORMED_MESSAGE = 'not well-formed'
+  private_constant :MALFORMED_MESSAGE
+
+  BINMODE_IO = Class.new(StringIO) do
+    attr_reader :binmode_called
+
+    def binmode
+      @binmode_called = true
+      super
+    end
+  end
+  private_constant :BINMODE_IO
+
+  def test_parse_rejects_non_lenex_root
+    error = assert_raises(::Lenex::Parser::ParseError) { Lenex::Parser.parse(NON_LENEX_ROOT_XML) }
+    assert_equal 'Root element must be LENEX', error.message
+  end
+
+  def test_parse_wraps_nokogiri_syntax_errors
+    syntax_error = Nokogiri::XML::SyntaxError.new(MALFORMED_MESSAGE)
+
+    Nokogiri::XML::Document.stub(:parse, ->(*) { raise syntax_error }) do
+      error = assert_raises(::Lenex::Parser::ParseError) { Lenex::Parser.parse('<LENEX/>') }
+      assert_equal MALFORMED_MESSAGE, error.message
+    end
+  end
+
+  def test_ensure_io_enables_binmode_on_readable_sources
+    io = BINMODE_IO.new('<LENEX/>')
+
+    assert_same io, Lenex::Parser.send(:ensure_io, io)
+    assert io.binmode_called
+  end
+end
